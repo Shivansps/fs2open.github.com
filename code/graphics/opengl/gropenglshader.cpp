8dc013eee18f8f1683b8ebda085e817acff32428
Salvador Cipolla <shivanuo@hotmail.com>
2025-08-27 19:30:35 -0300

    fix some gl errors on opengl es

--- code/graphics/opengl/gropenglshader.cpp
+++ code/graphics/opengl/gropenglshader.cpp
@@ -177,11 +177,13 @@ static SCP_string opengl_shader_get_header(shader_type type_id, int flags, bool 
 	sflags << "#define OPENGL\n";
 	#else
 	sflags << "#version " << GLSL_version << " es\n"; 
+	sflags << "#extension GL_EXT_clip_cull_distance : enable" << "\n"; 
 	sflags << "precision highp float;" << "\n";
 	sflags << "precision highp int;" << "\n"; 
 	sflags << "precision highp sampler2D;" << "\n";
 	sflags << "precision highp sampler2DArray;" << "\n";
 	sflags << "precision highp samplerCube;" << "\n";
+	sflags << "precision highp samplerBuffer;" << "\n";
 	sflags << "#define OPENGL\n";
 	#endif
 	if (Detail.lighting < 3) {
